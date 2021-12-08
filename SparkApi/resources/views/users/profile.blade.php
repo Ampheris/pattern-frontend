166af0b42e3c2bf81482ab0358b36cb0fb69f8a0
Your Name <veronica-a@live.se>
2021-12-08 14:03:16 +0000

    fix subscription

--- SparkApi/resources/views/users/profile.blade.php
+++ SparkApi/resources/views/users/profile.blade.php
@@ -2,10 +2,11 @@
 
 @section('content')
 <div class="container">
+    @if (isset($subscription))
     <p>Start: {{ $subscription['start_date'] }}</p>
     <p>Renew: {{ $subscription['renewal_date'] }}</p>
     <p>Stop: {{ $subscription['cancelation_date'] }}</p>
-
+    @endif
     <a href="{{ route('subscription') }}">Hantera</a>
     <p>{{ $balance }}</p>
     <a href="{{ route('balance') }}">Fyll på</a>
