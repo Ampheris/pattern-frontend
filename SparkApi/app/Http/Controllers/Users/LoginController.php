d6a5724cdeb130bc82fb6f38f3dc98f7ed6eb51b
Your Name <veronica-a@live.se>
2021-12-13 12:23:38 +0000

    User to user

--- SparkApi/app/Http/Controllers/Users/LoginController.php
+++ SparkApi/app/Http/Controllers/Users/LoginController.php
@@ -14,7 +14,7 @@ class LoginController extends Controller
      */
     public function index()
     {
-        return view('Users.loginGithub');
+        return view('users.loginGithub');
     }
 
         /**
