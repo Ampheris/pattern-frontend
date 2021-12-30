f340cc51949b71efa0a4049f623b9384cd80f892
Your Name <veronica-a@live.se>
2021-12-30 13:36:33 +0000

    navbar

--- SparkApi/resources/views/auth/layouts/app.blade.php
+++ SparkApi/resources/views/auth/layouts/app.blade.php
@@ -15,10 +15,10 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     @yield('styles')
 
@@ -29,6 +29,7 @@
 </head>
 <body>
     <div id="app">
+            @include('auth/layouts/navbar')
         <div class="menu-overlay"></div>
         <main class="py-4">
             @yield('content')
